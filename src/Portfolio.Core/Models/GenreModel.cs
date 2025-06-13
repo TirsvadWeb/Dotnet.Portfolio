562832b5d5776ae5441df20a6052573512aab151
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-06-13 15:59:27 +0800

    Add new Portfolio.Core

---
+++ src/Portfolio.Core/Models/GenreModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Portfolio.Core.Interfaces;
+
+namespace Portfolio.Core.Models;
+
+public class GenreModel : IHasGuid
+{
+    #region Properties
+    [Key]
+    public Guid Guid { get; set; }
+    [Required]
+    [StringLength(20)]
+    public required string Name
+    {
+        get; set;
+    }
+    #endregion
+    public GenreModel()
+    {
+    }
+}
---
+++ src/Portfolio.Core/Repository/RepositoryBase.cs
@@ -0,0 +1,34 @@
+﻿using Portfolio.Core.Interfaces;
+
+namespace Portfolio.Core.Repository;
+
+public abstract class RepositoryBase<T> where T : class, IHasGuid
+{
+    public List<T> Items { get; set; } = [];
+
+    public Task AddAsync(T item)
+    {
+        Items.Add(item);
+        return Task.CompletedTask;
+    }
+
+    public Task<List<T>> GetAsync() =>
+    Task.FromResult(Items);
+
+    public Task<T?> GetAsync(Guid guid) =>
+        Task.FromResult(Items.FirstOrDefault(item => item.Guid == guid));
+
+    public Task UpdateAsync(T item)
+    {
+        var index = Items.FindIndex(x => x.Guid == item.Guid);
+        if (index != -1)
+            Items[index] = item;
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(T item)
+    {
+        Items.Remove(item);
+        return Task.CompletedTask;
+    }
+}
