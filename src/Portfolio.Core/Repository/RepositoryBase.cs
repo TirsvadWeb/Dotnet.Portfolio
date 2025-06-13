af852b8cb46877eb4d038123adc6f6b4b7d1b20e
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-06-13 16:43:46 +0800

    Add lock in repositoryBase

--- src/Portfolio.Core/Repository/RepositoryBase.cs
+++ src/Portfolio.Core/Repository/RepositoryBase.cs
@@ -5,30 +5,51 @@ namespace Portfolio.Core.Repository;
 public abstract class RepositoryBase<T> where T : class, IHasGuid
 {
     public List<T> Items { get; set; } = [];
+    protected readonly object _lock = new();
 
     public Task AddAsync(T item)
     {
-        Items.Add(item);
+        lock (_lock)
+        {
+            Items.Add(item);
+        }
         return Task.CompletedTask;
     }
 
-    public Task<List<T>> GetAsync() =>
-    Task.FromResult(Items);
+    public Task<List<T>> GetAsync()
+    {
+        lock (_lock)
+        {
+            // Return a copy to avoid external modification
+            return Task.FromResult(new List<T>(Items));
+        }
+    }
 
-    public Task<T?> GetAsync(Guid guid) =>
-        Task.FromResult(Items.FirstOrDefault(item => item.Guid == guid));
+    public Task<T?> GetAsync(Guid guid)
+    {
+        lock (_lock)
+        {
+            return Task.FromResult(Items.FirstOrDefault(item => item.Guid == guid));
+        }
+    }
 
     public Task UpdateAsync(T item)
     {
-        var index = Items.FindIndex(x => x.Guid == item.Guid);
-        if (index != -1)
-            Items[index] = item;
+        lock (_lock)
+        {
+            var index = Items.FindIndex(x => x.Guid == item.Guid);
+            if (index != -1)
+                Items[index] = item;
+        }
         return Task.CompletedTask;
     }
 
     public Task DeleteAsync(T item)
     {
-        Items.Remove(item);
+        lock (_lock)
+        {
+            Items.Remove(item);
+        }
         return Task.CompletedTask;
     }
 }
