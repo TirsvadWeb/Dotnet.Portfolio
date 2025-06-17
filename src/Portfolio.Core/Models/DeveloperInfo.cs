41bcb16769bab98e50a57dd75b15c8b425eb4848
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-06-17 11:35:33 +0800

    Change index pag e to show portfolio items

---
+++ src/Portfolio.Core/Models/DeveloperInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Core.Models;
+
+public class DeveloperInfo
+{
+    public int ID { get; set; }
+
+    [Required, StringLength(50)]
+    public string Name { get; set; } = string.Empty;
+
+    [StringLength(500)]
+    public string Description { get; set; } = string.Empty;
+
+    [Url, StringLength(2048)]
+    public string? ProfileImageUrl { get; set; }
+
+    [Url, StringLength(2048)]
+    public string? SocialBannerUrl { get; set; }
+}
---
+++ src/Portfolio/Data/Migrations/20250616072358_AddDeveloperInfo.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Portfolio.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeveloperInfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DeveloperInfo",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 500, nullable: false),
+                    ProfileImageUrl = table.Column<string>(type: "TEXT", maxLength: 2048, nullable: true),
+                    SocialBannerUrl = table.Column<string>(type: "TEXT", maxLength: 2048, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeveloperInfo", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DeveloperInfo");
+        }
+    }
+}
