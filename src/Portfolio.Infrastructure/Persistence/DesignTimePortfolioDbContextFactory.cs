c71c572856f790a5cf3bfd60b8bfcaea3c5604fb
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-06-22 22:02:36 +0800

    Add  DbSet<ApplicationUser>  to DbContext

---
+++ src/Portfolio.Infrastructure/Persistence/DesignTimePortfolioDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Portfolio.Infrastructure.Persistence;
+
+public class DesignTimePortfolioDbContextFactory : IDesignTimeDbContextFactory<PortfolioDbContext>
+{
+    public PortfolioDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<PortfolioDbContext>();
+        // Use your actual connection string here
+        optionsBuilder.UseSqlite("Data Source=data/portfolio.db");
+
+        return new PortfolioDbContext(optionsBuilder.Options);
+    }
+}
